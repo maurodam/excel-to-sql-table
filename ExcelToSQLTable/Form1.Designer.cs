21642feb76b64af367134d04d36716dac6c9892a
Damiano Mauro <d.mauro@teamsystem.com>
2023-02-16 21:53:52 +0100

    Add project files.

---
+++ ExcelToSQLTable/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace ExcelToSQLTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1522, 1124);
+            this.splitContainer1.SplitterDistance = 575;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Seleziona il file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1492, 520);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(261, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Genera script SQL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseWaitCursor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 76);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(1498, 457);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome tabella";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "#temp";
+            this.textBox1.Size = new System.Drawing.Size(243, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1522, 1124);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Excel columns to SQL temp table";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ExcelToSQLTable/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using DataTable = System.Data.DataTable;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace ExcelToSQLTable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Crea un'istanza del controllo OpenFileDialog
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Imposta il filtro di selezione file su Excel
+            openFileDialog1.Filter = "Excel Files|*.xlsx;*.xls";
+
+            // Mostra la finestra di dialogo per la selezione del file
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            // Se l'utente ha selezionato un file
+            if (result == DialogResult.OK)
+            {
+                // Crea un'istanza dell'applicazione Excel
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+                // Imposta l'opzione di visualizzazione dell'applicazione Excel su false
+                excel.Visible = false;
+
+                // Apri il file Excel selezionato
+                Workbook workbook = excel.Workbooks.Open(openFileDialog1.FileName);
+
+                // Seleziona il foglio di lavoro desiderato
+                Worksheet worksheet = workbook.Sheets[1];
+
+                // Ottieni l'intervallo di celle utilizzato dal foglio di lavoro
+                Range range = worksheet.UsedRange;
+
+                // Crea un oggetto DataTable per contenere i dati dal foglio di lavoro
+                DataTable dataTable = new DataTable();
+
+                // Leggi i dati dall'intervallo di celle e inseriscili nella DataTable
+                for (int row = 1; row <= range.Rows.Count; row++)
+                {
+                    DataRow dataRow = dataTable.NewRow();
+                    for (int col = 1; col <= range.Columns.Count; col++)
+                    {
+                        if (row == 1)
+                        {
+                            // Aggiungi le intestazioni delle colonne alla DataTable
+                            dataTable.Columns.Add(range.Cells[row, col].Value2.ToString());
+                        }
+                        else
+                        {
+                            // Aggiungi i dati delle celle alla DataTable
+                            dataRow[col - 1] = range.Cells[row, col].Value2;
+                        }
+                    }
+                    if (row != 1)
+                    {
+                        dataTable.Rows.Add(dataRow);
+                    }
+                }
+
+                // Chiudi il file Excel
+                workbook.Close(false);
+                excel.Quit();
+
+                // Mostra i dati nella DataGridView
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private String CreateTable()
+        {
+            // Crea un oggetto DataTable vuoto con il nome della DataGridView
+            DataTable dataTable = new DataTable(dataGridView1.Name);
+
+            // Aggiungi le colonne alla DataTable
+            foreach (DataGridViewColumn dataGridViewColumn in dataGridView1.Columns)
+            {
+                dataTable.Columns.Add(dataGridViewColumn.Name);
+            }
+
+            // Ottieni i nomi delle colonne come stringa separate da virgole
+            string columnNames = string.Join(", ", dataTable.Columns.Cast<DataColumn>().Select(c => $"[{c.ColumnName}] NVARCHAR(MAX)"));
+
+            // Costruisci lo script CREATE TABLE
+            string createTableScript = $"CREATE TABLE #temp ({columnNames})";
+
+            // Mostra lo script nella casella di testo
+            return createTableScript;
+        }
+
+
+        void generateScript()
+        {
+            var tableName = string.IsNullOrEmpty(textBox1.Text.Trim()) ? "#temp" : textBox1.Text.Trim();
+
+            DataTable dataTable = new DataTable(dataGridView1.Name);
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                dataTable.Columns.Add(column.Name);
+            }
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                object[] rowData = new object[row.Cells.Count];
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    rowData[i] = row.Cells[i].Value;
+                }
+                dataTable.Rows.Add(rowData);
+            }
+
+            string sqlOutput = "";
+            foreach (DataRow row in dataTable.Rows)
+            {
+                string rowValues = "";
+                for (int i = 0; i < dataTable.Columns.Count; i++)
+                {
+                    rowValues += row[i].ToString().Replace("'", "''").Trim();
+
+                    if (i < dataTable.Columns.Count - 1)
+                    {
+                        rowValues += "','";
+                    }
+                }
+                var sqlQuery = "INSERT INTO " + tableName + $" VALUES ('{rowValues}'){Environment.NewLine}";
+
+                sqlOutput += sqlQuery;
+            }
+
+            textBox2.Text = CreateTable() + Environment.NewLine + Environment.NewLine + sqlOutput;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            generateScript();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+    }
+}
